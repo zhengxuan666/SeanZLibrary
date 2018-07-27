a9dc4a70055cb6a66afe7c154e80a924d40df053
SeanZ <307778252@qq.com>
2018-07-27 10:46:43 +0800

    update 1.0.2 修改包名

--- app/src/main/java/com/seanz/ui/MainActivity.java
+++ app/src/main/java/com/seanz/ui/MainActivity.java
@@ -3,7 +3,8 @@ package com.seanz.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.seanz.library.R;
+import com.seanz.demo.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
